ba1fef298fe994d9547650b162691c6eac2748a8
Shane Banks <shane.banks02@gmail.com>
2022-04-20 14:28:08 -0500

    Excercise 0 complete

--- code.js
+++ code.js
@@ -1,7 +1,18 @@
 // helloWorld function 
-function helloWorld() {
+
+/* function helloWorld() {
+//     return "Hello, World!";
+// }*/
+
+
+const helloWorld = function() {
     return "Hello, World!";
 }
+
+
+
+
+
 // function plusOne(num) {
 //     if (typeof num === "number") {
 //         return num + 1;
